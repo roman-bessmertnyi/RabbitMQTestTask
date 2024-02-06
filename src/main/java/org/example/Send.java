cc8eceb0db87ea06b9955e0d18ffb10bc6617d3d
robe0519 <roman.bessmertnyi@netcracker.com>
2024-02-06 16:11:13 +0200

    Rabbit Send test

---
+++ src/main/java/org/example/Send.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+public class Send {
+    private final static String QUEUE_NAME = "hello";
+    private final static String HOST = "localhost";
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(HOST);
+        try (Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel()) {
+
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+}
